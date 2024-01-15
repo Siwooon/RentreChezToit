47bbe124d34d4be6dcc028fbd85a3c0ee28b87ba
Siwooon <simonquachpro@gmail.com>
2024-01-15 18:56:26 +0100

    Start

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/user', [UserController::class, 'view']);
+Route::get('/ads', [AdsController::class, 'view']);
+Route::get('/login', [LoginController::class, 'view']);
+Route::get('/Register', [RegisterController::class, 'view']);
+Route::get('/ads/Create', [CreateController::class, 'view']);
+
+
